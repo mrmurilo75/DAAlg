bd6b12ee65b7281e107c7672a0adacd4e8306e8e
Murilo Rosa <mrmurilo75@gmail.com>
2020-11-20 03:01:22 +0000

    @P06/022 :add BST check & refactor to O(n^2) from O(n^4)

--- P06/022/D22.java
+++ P06/022/D22.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 class Node{
-	boolean isBlack;
-	boolean isNull;
-	int value, blackCount;
+	boolean isBlack, isNull;
+	int value, blackCount, max=Integer.MAX_VALUE, min=Integer.MIN_VALUE;
 	Node left, right;
 
 	Node(int v){
@@ -12,7 +11,30 @@ class Node{
 		value=Math.abs(v);
 	}
 
-	boolean followRedProperty(){
+	void initializeBST(){
+		if(this.isNull)
+			return;
+		this.max=this.value;
+		this.min=this.value;
+	}
+
+	boolean followsBSTProperty(){
+		if(this.isNull)
+			return true;
+		if(!this.left.isNull){
+			this.left.max=this.value;
+			this.left.min=this.min;
+		}
+		if(!this.right.isNull){
+			this.right.max=this.max;
+			this.right.min=this.value;
+		}
+			if(!(right.value>=right.min && right.value<=right.max) || !(left.value<=left.max && left.value>=left.min))
+				return false;
+		return true;
+	}
+
+	boolean followsRedProperty(){
 		if(isBlack)
 			return true;
 		if(this.left.isBlack && this.right.isBlack)
@@ -48,38 +70,27 @@ public class D22{
 		return 1 + size(t.left) + size(t.right);
 	}
 
-	static boolean hasBlackProperty(Node root){
+	static boolean hasAllProperties(Node root){
 		root.blackCount=1;
-		return (countBlacks(root)>0);
+		//root.initializeBST();
+		return (countBlacks(root, 1)>0);
 	}
-	static int countBlacks(Node node){
-		int left, right;
-		if(node.left.isBlack)
-			node.left.blackCount=node.blackCount+1;
-		else
-			node.left.blackCount=node.blackCount;
-		if(node.right.isBlack)
-			node.right.blackCount=node.blackCount+1;
-		else
-			node.right.blackCount=node.blackCount;
-		if(node.left.isNull)
-			left=node.left.blackCount;
-		else
-			left=countBlacks(node.left);
-		if(node.right.isNull)
-			right=node.right.blackCount;
-		else
-			right=countBlacks(node.right);
-		return ( (left==right)? left : 0 );
-	}
-
-	static boolean hasRedProperty(Node root){
-		if(root.isNull)
-			return true;
-		if(root.followRedProperty())
-			return (hasRedProperty(root.left) && hasRedProperty(root.right));
-		return false;
-		
+	static int countBlacks(Node node, int parentBlackCount){
+		//System.out.print(node.value);
+		if(node.isNull)
+			return parentBlackCount+1;
+		if(!node.followsBSTProperty())
+			return 0;
+		//System.out.print("\tBST true");
+		node.blackCount=parentBlackCount;
+		if(node.isBlack)
+			node.blackCount++;
+		if(!node.followsRedProperty())
+			return 0;
+		//System.out.print("\tRedP true");
+		//System.out.println();
+		int leftC, rightC;
+		return ( ((leftC=countBlacks(node.left, node.blackCount))==0 || (rightC=countBlacks(node.right, node.blackCount))==0 || leftC!=rightC)? 0 : leftC );
 	}
 
 	public static void main(String args[]){
@@ -87,7 +98,7 @@ public class D22{
 		int n= in.nextInt();
 		for(int i=0; i<n; i++){
 			Node root=readPreOrder(in);
-			if(root.isBlack &&  hasRedProperty(root) && hasBlackProperty(root))
+			if(root.isBlack && hasAllProperties(root))
 				System.out.println("SIM");
 			else
 				System.out.println("NAO");
